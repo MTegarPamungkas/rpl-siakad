31951c5dee55480c6deb0917237994be8f2ea004
Tegar Pamungkas <mtegarpamungkas111@gmail.com>
2023-06-04 20:55:18 +0700

    temporarily remove permissions

--- src/page/berita/addBerita.tsx
+++ src/page/berita/addBerita.tsx
@@ -1,5 +1,4 @@
 import moment from "moment";
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { EditorQuill } from "../../components";
 import BeritaDataService from "../../service/berita";
@@ -46,50 +45,50 @@ const addBerita = () => {
     }
   };
 
-  useEffect(() => {
-    if (role != "guru") {
-      navigate("/informasi");
-    }
-  }, [role]);
+  // useEffect(() => {
+  //   if (role != "guru") {
+  //     navigate("/informasi");
+  //   }
+  // }, [role]);
   return (
     <div>
-      {role != "guru" ? (
+      {/* {role != "guru" ? (
         ""
-      ) : (
-        <div className="p-4">
-          <form onSubmit={addBerita}>
-            <input
-              type="text"
-              disabled
-              placeholder="Title"
-              value={date}
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-200 rounded mb-2"
-            />
-            <input
-              type="text"
-              placeholder="Title"
-              onChange={(e) => setTitleBerita(e.target.value)}
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-200 rounded mb-2"
-            />
-            <input
-              type="text"
-              placeholder="Keterangan"
-              onChange={(e) => setKeteranganBerita(e.target.value)}
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-200 rounded mb-2"
-            />
-            <EditorQuill />
+      ) : ( */}
+      <div className="p-4">
+        <form onSubmit={addBerita}>
+          <input
+            type="text"
+            disabled
+            placeholder="Title"
+            value={date}
+            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-200 rounded mb-2"
+          />
+          <input
+            type="text"
+            placeholder="Title"
+            onChange={(e) => setTitleBerita(e.target.value)}
+            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-200 rounded mb-2"
+          />
+          <input
+            type="text"
+            placeholder="Keterangan"
+            onChange={(e) => setKeteranganBerita(e.target.value)}
+            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-200 rounded mb-2"
+          />
+          <EditorQuill />
 
-            <div className="flex justify-end">
-              <button
-                type="submit"
-                className="bg-blue-500 mt-2 text-right hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              >
-                Tambah
-              </button>
-            </div>
-          </form>
-        </div>
-      )}
+          <div className="flex justify-end">
+            <button
+              type="submit"
+              className="bg-blue-500 mt-2 text-right hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Tambah
+            </button>
+          </div>
+        </form>
+      </div>
+      {/* )} */}
     </div>
   );
 };

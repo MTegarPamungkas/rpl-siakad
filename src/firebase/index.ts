124c76a76dfffc14b745b5d75264ca06f7166900
Tegar Pamungkas <mtegarpamungkas111@gmail.com>
2023-06-04 20:37:01 +0700

    move to env

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9f3xtsw1j0JEqJqoOZvwmtqbOVP7sd7k",
-  authDomain: "rpl-siakad.firebaseapp.com",
-  projectId: "rpl-siakad",
-  storageBucket: "rpl-siakad.appspot.com",
-  messagingSenderId: "106420963869",
-  appId: "1:106420963869:web:1b149446642517c29e54f4",
-  measurementId: "G-4YGNR7DWRR"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
